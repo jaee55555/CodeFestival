d50ea7676f8e007fc5a2afa6f2c76668cf9c5155
jaee55555 <jaeeO55555@gmail.com>
2024-09-04 16:10:31 +0900

    Solved chapter 1

---
+++ Python/Section_1/Q4.py
@@ -0,0 +1,12 @@
+#다음 변수 a를 print(type(a))로 넣었을 때 출력될 값과의 연결이 맞지 않은 것은?
+
+#1) a=1, 출력: class 'int'
+#2) a=2.22, 출력: class 'float'
+#3) a='p', 출력: class 'char'
+#4) a=[1,2,3], 출력: class 'list'
+
+#답: 3번
+#class 'str'로 나와야 함
+
+a='p'
+print(type(a))
---
+++ Python/Section_1/Q9.py
@@ -0,0 +1,12 @@
+#다음 소스코드를 완성하여 날짜와 시간을 출력하시오
+#sep와 end를 활용한 출력방법
+
+year = '2019'
+month = '04'
+day = '26'
+hour = '11'
+minute = '34'
+second = '27'
+
+print(year, month, day, sep="/", end=" ")
+print(hour, minute, second, sep=":")
